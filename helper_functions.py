3d81d6eb185d9e6630a9f92cf43eb7c94f40b466
Remy Flatt <remy.flatt@qotient.com>
2023-03-09 16:47:51 +1300

    Add na_values_table_for_dataframe to helper functions

---
+++ helper_functions.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+def plot_loss_curves(history):
+  """
+  Returns separate loss curves for training and validation metrics.
+  Args:
+    history: TensorFlow model History object (see: https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/History)
+  """
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+
+  accuracy = history.history['accuracy']
+  val_accuracy = history.history['val_accuracy']
+
+  epochs = range(len(history.history['loss']))
+
+  # Plot loss
+  plt.plot(epochs, loss, label='training_loss')
+  plt.plot(epochs, val_loss, label='val_loss')
+  plt.title('Loss')
+  plt.xlabel('Epochs')
+  plt.legend()
+
+  # Plot accuracy
+  plt.figure()
+  plt.plot(epochs, accuracy, label='training_accuracy')
+  plt.plot(epochs, val_accuracy, label='val_accuracy')
+  plt.title('Accuracy')
+  plt.xlabel('Epochs')
+  plt.legend();
+
+
+def save_model_to_drive(project_name, model):
+  path = "/content/drive/MyDrive/Kaggle/" + project_name + "/" + model.name
+  model.save(path)
+
+def na_values_table_for_dataframe(df, count=20):
+  """
+  For the dataframe output the top {count} values of features with NA values
+  """
+  total = df.isnull().sum().sort_values(ascending=False)
+  percent = (df.isnull().sum()/df.isnull().count()).sort_values(ascending=False)
+  missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+
+  return missing_data.head(count)
